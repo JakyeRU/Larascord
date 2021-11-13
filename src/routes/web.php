81fccc73d9afe115887c8b40f8b166674bb82179
Jakye <jakyeru@gmail.com>
2021-11-14 00:12:21 +0200

    Added dashboard route.

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,4 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 require __DIR__ .'/auth.php';
