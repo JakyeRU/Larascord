3365758e9d8307cfe6041ce79ae1f56d77e35b6f
Jakye <jakyeru@gmail.com>
2021-11-24 17:36:02 +0200

    resolves #1

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/login', 'https://discord.com/oauth2/authorize?client_id=' . env("DISCORD_CLIENT_ID") . '&redirect_uri=' . env("DISCORD_REDIRECT_URI") . '&response_type=code&scope=' . env("DISCORD_SCOPE"))->name('login');
+Route::redirect('/login', 'https://discord.com/oauth2/authorize?client_id=' . env("DISCORD_CLIENT_ID") . '&redirect_uri=' . env("DISCORD_REDIRECT_URI") . '&response_type=code&scope=' . implode('%20', explode('&', env("DISCORD_SCOPE"))))->name('login');
 
 Route::group(['prefix' => 'larascord'], function() {
     Route::get('/callback', [\App\Http\Controllers\DiscordController::class, 'login'])->name('larascord.login');
