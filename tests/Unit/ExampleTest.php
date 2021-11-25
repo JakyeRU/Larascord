7cd60b3f47c36ca25c3504fb3bcf7b6d44499e5a
Jakye <jakyeru@gmail.com>
2021-11-25 14:40:24 +0200

    Added an example test.

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jakyeru\Larascord\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function example_test()
+    {
+        $this->assertTrue(true);
+    }
+}
