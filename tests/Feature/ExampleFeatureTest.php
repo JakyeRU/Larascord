b2faa7762ea7a67ecbfe3ba6c4e83edaef1e1fdc
Jakye <jakyeru@gmail.com>
2021-11-25 14:42:26 +0200

    Added an example feature test.

---
+++ tests/Feature/ExampleFeatureTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jakyeru\Larascord\Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleFeatureTest extends TestCase
+{
+    /** @test */
+    public function example_feature_test()
+    {
+        $this->assertTrue(true);
+    }
+}
