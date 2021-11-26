6d01546098ded2f04ba53a2d142874b0af469860
Jakye <jakyeru@gmail.com>
2021-11-26 16:06:10 +0200

    Updated web.php routes.

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -10,4 +21,4 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__ .'/auth.php';
+require __DIR__.'/auth.php';
