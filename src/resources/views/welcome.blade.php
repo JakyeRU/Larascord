c3b7fc24fc2e45a66d52ad17d183425f2c76115b
Jakye <jakyeru@gmail.com>
2021-11-27 22:51:01 +0200

    Changed the alert in welcome.blade.php.

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -41,8 +41,14 @@
 
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
         @if(session()->has('error'))
-            <div style="padding: 1rem; background-color: rgba(253, 232, 232, 1); color: rgba(200,30,30,1); margin-bottom: 1rem; border-radius: .5rem">
-                <strong>Something went wrong.</strong> {!! session()->get('error') !!}
+            <div class="w-full my-3">
+                <div class="bg-red-100 rounded-lg p-4 mb-4 text-sm text-red-700" role="alert">
+                    <svg class="w-5 h-5 inline mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+
+                    <span class="font-medium">
+                    <strong>Something went wrong!</strong>
+                </span> {!! session()->get('error') !!}
+                </div>
             </div>
         @endif
 
