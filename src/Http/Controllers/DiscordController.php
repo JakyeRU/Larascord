6c23d9586db3bfa8313593f74a577f18f6c966c4
JakyeRU <jakyeru@gmail.com>
2022-01-11 19:24:14 +0200

    Updated namespace.

--- src/Http/Controllers/DiscordController.php
+++ src/Http/Controllers/DiscordController.php
@@ -1,13 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace Jakyeru\Larascord\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Validation\ValidationException;
 
 class DiscordController extends Controller
 {
