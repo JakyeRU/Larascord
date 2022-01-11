97d91c13c3352cef62b61f0bf12212791c4d7024
JakyeRU <jakyeru@gmail.com>
2022-01-11 18:45:47 +0200

    Updated config.php with the required values.

--- src/config/config.php
+++ src/config/config.php
@@ -1,1 +1,72 @@
 <?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application ID
+    |--------------------------------------------------------------------------
+    |
+    | This is the ID of your Discord application.
+    |
+    */
+
+    'client_id' => env('DISCORD_CLIENT_ID', null),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Secret
+    |--------------------------------------------------------------------------
+    |
+    | This is the secret of your Discord application.
+    |
+    */
+
+    'client_secret' => env('DISCORD_CLIENT_SECRET', null),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Grant Type
+    |--------------------------------------------------------------------------
+    |
+    | This is the grant type of your Discord application. It must be set to "authorization_code".
+    |
+    */
+
+    'grant_type' => env('DISCORD_GRANT_TYPE', 'authorization_code'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Redirect URI
+    |--------------------------------------------------------------------------
+    |
+    | This is the URI that Discord will redirect to after the user authorizes your application.
+    |
+    */
+
+    'redirect_uri' => env('APP_URL', 'http://localhost:8000') . '/' . config('larascord.prefix') . '/callback',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Scopes
+    |--------------------------------------------------------------------------
+    |
+    | These are the OAuth2 scopes of your Discord application.
+    |
+    */
+
+    'scopes' => env('DISCORD_SCOPES', 'identify&email'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Route Prefix
+    |--------------------------------------------------------------------------
+    |
+    | This is the prefix that Larascord will use for its routes.
+    | For example, the prefix "larascord" will result in the route "https://domain.com/larascord/login".
+    |
+    */
+
+    'prefix' => 'larascord',
+
+];
