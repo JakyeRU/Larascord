c3f5cdce75528b408195336c0d83329f2b8ed613
Jakye <jakyeru@gmail.com>
2023-02-20 20:16:18 +0200

    Updated web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -27,5 +27,3 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-require __DIR__.'/auth.php';
