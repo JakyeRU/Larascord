816f79188a7b7769b0cf111831832d824bb1ee82
Jakye <jakyeru@gmail.com>
2023-02-20 21:19:33 +0200

    Added success alert in welcome.blade.php

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -44,8 +44,20 @@
                     <svg class="w-5 h-5 inline-flex mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
 
                     <span class="font-medium">
-                    <strong>Something went wrong!</strong>
+                    <strong>{{ __('Something went wrong!') }}</strong>
                 </span> {!! session()->get('error') !!}
+                </div>
+            </div>
+        @endif
+
+        @if (session()->has('success'))
+            <div class="w-full my-3">
+                <div class="rounded-lg p-4 mb-4 text-sm mt-16" style="background-color: rgba(73, 255, 0, 0.1); color: rgba(73, 255, 0, 0.7); padding: 15px 15px;" role="alert">
+                    <svg class="w-5 h-5 inline-flex mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+
+                    <span class="font-medium">
+                    <strong>{{ __('Success') }}!</strong>
+                </span> {!! session()->get('success') !!}
                 </div>
             </div>
         @endif
