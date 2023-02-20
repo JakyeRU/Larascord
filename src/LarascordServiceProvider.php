841edd812faf656860a87307e602dd13423e65ee
Jakye <jakyeru@gmail.com>
2023-02-20 22:19:38 +0200

    Update LarascordServiceProvider.php
    Signed-off-by: Jakye <jakyeru@gmail.com>

--- src/LarascordServiceProvider.php
+++ src/LarascordServiceProvider.php
@@ -11,7 +11,7 @@ class LarascordServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.1.1';
+    const VERSION = '4.1.2';
 
     /*
      * Register the application services.
