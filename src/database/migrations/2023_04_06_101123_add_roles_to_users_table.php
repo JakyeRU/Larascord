4f44f101df67d622d31443ee43933eceaa11198c
Jakye <jakyeru@gmail.com>
2023-04-17 11:58:10 +0300

    Removed default value for column roles.

--- src/database/migrations/2023_04_06_101123_add_roles_to_users_table.php
+++ src/database/migrations/2023_04_06_101123_add_roles_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('roles')->default('{}');
+            $table->json('roles')->nullable();
         });
     }
 
