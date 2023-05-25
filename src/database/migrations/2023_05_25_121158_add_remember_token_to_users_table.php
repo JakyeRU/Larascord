b8237b327c20720eeff0c61acc0a518abd17a13c
Jakye <jakyeru@gmail.com>
2023-05-25 15:13:20 +0300

    Added 2023_05_25_121158_add_remember_token_to_users_table.php

--- src/database/migrations/2023_05_25_121158_add_remember_token_to_users_table.php
+++ src/database/migrations/2023_05_25_121158_add_remember_token_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->rememberToken();
+            $table->rememberToken()->after('refresh_token');
         });
     }
 
