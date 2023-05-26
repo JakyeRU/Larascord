e4a0fdfbe89752fe71c4a37ceaca0d6ae341020e
Jakye <jakyeru@gmail.com>
2023-05-26 20:16:49 +0300

    Updated User.php to use InteractsWithDiscord.

--- src/Models/User.php
+++ src/Models/User.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Jakyeru\Larascord\Traits\InteractsWithDiscord;
 use Laravel\Sanctum\HasApiTokens;
 
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, InteractsWithDiscord;
 
     /**
      * The attributes that are mass assignable.
