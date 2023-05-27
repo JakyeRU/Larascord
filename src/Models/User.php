df4e9e007da66690b258c8a332270fc26a681f85
Jakye <jakyeru@gmail.com>
2023-05-27 09:07:48 +0300

    Removed InteractsWithDiscord.php

--- src/Models/User.php
+++ src/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, InteractsWithDiscord;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
