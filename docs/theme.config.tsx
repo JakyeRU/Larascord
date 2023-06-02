2d2ed607a62bf54eb95c0276406ee3308f520d36
Jakye <jakyeru@gmail.com>
2023-06-02 20:15:24 +0300

    Fixed title in theme.config.tsx

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Larascorda</span>,
+  logo: <span>Larascord</span>,
 
   navigation: true,
 
