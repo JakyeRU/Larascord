ada377c939127f9d236f5e0643bafed9377c4317
Jakye <jakyeru@gmail.com>
2023-07-20 09:39:37 +0300

    Updated version

--- src/LarascordServiceProvider.php
+++ src/LarascordServiceProvider.php
@@ -11,7 +11,7 @@ class LarascordServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '5.0.3';
+    const VERSION = '5.0.4';
 
     /*
      * Register the application services.
