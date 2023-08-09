7886b3beeaccde29ece6dc2536839a5e3d92b131
DamoFD <voshall@yahoo.com>
2023-08-08 18:16:55 -0700

    updated navigation layout with new method
    updated navigation layout with new method

--- src/resources/views/layouts/navigation.blade.php
+++ src/resources/views/layouts/navigation.blade.php
@@ -23,8 +23,8 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            @if(Auth::user()->avatar)
-                                <img class="h-8 w-8 rounded-full object-cover mr-2" src="https://cdn.discordapp.com/avatars/{{ Auth::user()->id }}/{{ Auth::user()->avatar }}.webp" alt="{{ Auth::user()->getTagAttribute() }}" />
+                            @if(Auth::user())
+                                <img class="h-8 w-8 rounded-full object-cover mr-2" src="{{Auth::user()->getAvatar('webp', 32)}}" alt="{{ Auth::user()->getTagAttribute() }}" />
                             @endif
 
                             <div style="display: flex; flex-direction: column; align-items: flex-start;">
