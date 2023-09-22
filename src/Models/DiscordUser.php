4259b56442a211f188955841597dc8b6a8a9a5f6
Jakye <jakyeru@gmail.com>
2023-09-22 10:34:13 +0300

    Fixed DiscordUser.php namespace

--- src/Models/DiscordUser.php
+++ src/Models/DiscordUser.php
@@ -1,14 +1,11 @@
 <?php
 
-namespace App\Models;
+namespace Jakyeru\Larascord\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DiscordUser extends Model
 {
-    use HasFactory;
-
     /**
      * The attributes that are mass assignable.
      */
