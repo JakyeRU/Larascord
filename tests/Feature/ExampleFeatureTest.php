065ca226439bae33beb0666e28e57a5103a14db4
Jakye <jakyeru@gmail.com>
2025-06-18 21:40:32 +0300

    Updated example test name.

--- tests/Feature/ExampleFeatureTest.php
+++ tests/Feature/ExampleFeatureTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 class ExampleFeatureTest extends TestCase
 {
     /** @test */
-    public function example_feature_test()
+    public function test_example_feature()
     {
         $this->assertTrue(true);
     }
